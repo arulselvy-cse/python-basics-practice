d532249272eebd9675fdcd6e150a09bf81e53538
arulselvy-cse <arulselvyr@gmail.com>
2026-01-26 18:23:08 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+name = "Arulselvy"
+age = 23
+height = 6.5
+is_student = true 
+print(name)
+  print(age)
+  print(height)
+  print(is_student)
+  print(type(name))
+  print(type(age))
+  print(type(height))
+  print(type(is_student))
