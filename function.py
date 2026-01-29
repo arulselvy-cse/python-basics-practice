35e08f2161144b024292fc10446ff50f2cbf9ad1
arulselvy-cse <arulselvyr@gmail.com>
2026-01-29 23:19:55 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def greet(name):
+  print("Hello",name)
+  
+  def check_age(age):
+    if age >= 15 :
+    print("Eligible")
+    else :
+    print("Not Eligible")
+    
+    greet("Arulselvy")
+    age(10)
+    
