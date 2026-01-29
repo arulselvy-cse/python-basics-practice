2211b73b759c22c06459b5ab1894fd047bd59353
arulselvy-cse <arulselvyr@gmail.com>
2026-01-29 22:55:56 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+students = ["madhu","sree","vicky"]
+marks = [10,20,30]
+print(students)
+print(marks)
+for name in student:
+print("Student name is:",name)
+for mark in marks :
+print("Mark:", mark)
